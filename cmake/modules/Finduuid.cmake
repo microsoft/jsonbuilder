d865461d522925c89aee4657fb66517cfc73f72c
Nick Bopp <nichbop@microsoft.com>
2020-01-24 23:03:48 -0800

    Finish improving install

--- cmake/modules/Finduuid.cmake
+++ cmake/modules/Finduuid.cmake
@@ -3,32 +3,38 @@
 
 include(AliasPkgConfigTarget)
 
+if (TARGET uuid::uuid)
+    return()
+endif()
+
 # First try and find with PkgConfig
 find_package(PkgConfig QUIET)
 if (PKG_CONFIG_FOUND)
-    pkg_check_modules(UUID REQUIRED IMPORTED_TARGET uuid)
-    if (TARGET PkgConfig::UUID)
-        alias_pkg_config_target(uuid::uuid PkgConfig::UUID)
+    pkg_check_modules(uuid REQUIRED IMPORTED_TARGET uuid)
+    if (TARGET PkgConfig::uuid)
+        alias_pkg_config_target(uuid::uuid PkgConfig::uuid)
         return()
     endif ()
 endif ()
 
 # If that doesn't work, try again with old fashioned path lookup, with some caching
-if (NOT (UUID_INCLUDE_DIR AND UUID_LIBRARY))
-    find_path(UUID_INCLUDE_DIR
+if (NOT (uuid_INCLUDE_DIR AND uuid_LIBRARY))
+    find_path(uuid_INCLUDE_DIR
         NAMES uuid/uuid.h)
-    find_library(UUID_LIBRARY
+    find_library(uuid_LIBRARY
         NAMES uuid)
 
     include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(UUID DEFAULT_MSG
-        UUID_LIBRARY
-        UUID_INCLUDE_DIR)
+    find_package_handle_standard_args(uuid DEFAULT_MSG
+        uuid_LIBRARY
+        uuid_INCLUDE_DIR)
 
-    mark_as_advanced(UUID_LIBRARY UUID_INCLUDE_DIR)
+    mark_as_advanced(uuid_LIBRARY uuid_INCLUDE_DIR)
 endif()
 
 add_library(uuid::uuid UNKNOWN IMPORTED)
 set_target_properties(uuid::uuid PROPERTIES
-    IMPORTED_LOCATION "${UUID_LIBRARY}"
-    IMPORTED_INCLUDE_DIRECTORIES "${UUID_INCLUDE_DIR}")
+    IMPORTED_LOCATION "${uuid_LIBRARY}"
+    IMPORTED_INCLUDE_DIRECTORIES "${uuid_INCLUDE_DIR}")
+
+set(uuid_FOUND TRUE)
